aeaf02ff3f4a1d30e2f0325504a045879a0236fb
cindy <2251261281@qq.com>
2022-05-10 15:16:29 +0800

    5.10 commit

--- functional_tests.py
+++ functional_tests.py
@@ -24,31 +24,57 @@ class NewVisitorTest(unittest.TestCase):
         header_text=self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do',header_text)
         
-        # She is invited to enter a to-do item straight away
-        inputbox=self.browser.find_element_by_id('id_new_item')
-        self.assertEqual(
-            inputbox.get_attribute('placeholder'),
-            'Enter a to-do item'
-            )
+        # # She is invited to enter a to-do item straight away
+        # inputbox = self.browser.find_element_by_id('id_new_item')
+        # self.assertEqual(
+        #     inputbox.get_attribute('placeholder'),
+        #     'Enter a to-do item'
+        #     )
 
-        # She types "Buy peacock feathers" into a text box (Edith's hobbby
-        # is tying fly-fishing lures)
-        inputbox.send_keys('Buy peacock feathers')
+        # # She types "Buy peacock feathers" into a text box (Edith's hobbby
+        # # is tying fly-fishing lures)
+        # inputbox.send_keys('Buy peacock feathers')
 
-        # when she hits enter, the page updates, and now the page lists
-        # "1: Buy peacock feathers" as an item in a to-so list
+        # # when she hits enter, the page updates, and now the page lists
+        # # "1: Buy peacock feathers" as an item in a to-so list
+        
+        # inputbox.send_keys(Keys.ENTER)
+        # time.sleep(1)
+
+        # table=self.browser.find_element_by_id('id_list_table')
+        # rows=table.find_elements_by_tag_name('tr')
+        # # self.assertTrue(
+        # #     any(row.text == '1: Buy peacock feathers' for row in rows),
+        # #     f"New to-do item did not appear in table. Contents were:\n{table.text}"
+        # #     )
+        # self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        
+        # There is still a text box inviting her to add another item. She
+        # enters "Use peacock fathers to make a fly" (Edith is very methdical)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        table=self.browser.find_element_by_id('id_list_table')
+        # The page updates again, and now shows both items on her list
+        table = self.browser.find_element_by_id('id_list_table')
         rows=table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            "New to-do item did not appear in table"
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn(
+            '2: Use peacock feathers to make a fly',
+            [row.text for row in rows]
             )
-        # There is still a text box inviting her to add another item. She
-        # enters "Use peacock fathers to make a fly" (Edith is very methdical)
+
+        # Edith wonders whether the site will remember her list. Then she sees
+        # that the site has generated a unique URL for her -- there is some
+        # explanatory text to that effect.
         self.fail('Finish the test!')
+        
+        # She visits that URL - her to-do list is still there.
+
+        # Satisfied, she goes back to sleep
+
+        
 
 
 if __name__=='__main__':
@@ -58,14 +84,6 @@ if __name__=='__main__':
 
 
 
-# The page updates again, and now shows both items on her list
 
-# Edith wonders whether the site will remember her list. Then she sees
-# that the site has generated a unique URL for her -- there is some
-# explanatory text to that effect.
-
-# She visits that URL - her to-do list is still there.
-
-# Satisfied, she goes back to sleep
 
 #browser.quit()
